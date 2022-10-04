2a332df6841913fea129dd5cbde94b2a9d1a246a
wagner guimaraes <wagnerluizsg@gmail.com>
2022-10-04 12:09:57 -0300

    update 2/2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Brainn from './Brainn/Brainn';
+import styles from './styles/App.module.css'
+
+
+function App() {
+  return (
+    <div className={styles.app_container}>
+      <Brainn />
+    </div>
+  );
+}
+
+export default App;
